f37a6d6619498834086d4f412eebaea1eee36246
SulthanRafif <rsulthan5@gmail.com>
2021-10-22 15:06:50 +0700

    First Commit

---
+++ app/src/main/java/com/example/aplikasikeuangan/Pengaturan.java
@@ -0,0 +1,14 @@
+package com.example.aplikasikeuangan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pengaturan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengaturan);
+    }
+}
